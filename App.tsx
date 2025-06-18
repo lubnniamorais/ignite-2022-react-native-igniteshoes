5c7d6b1fbc442da54a636c14bcdb1279e6072022
lubnniamorais <lubnnia@gmail.com>
2025-06-18 10:26:57 -0300

    feat: Install OneSignal plugin

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import {OneSignal} from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
@@ -9,6 +10,8 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+// Antes de exportarmos a função devemos inicializar o OneSignal passando o app id
+OneSignal.initialize("5ca1c259-ed23-418e-a1fe-464cce22ea58");
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
