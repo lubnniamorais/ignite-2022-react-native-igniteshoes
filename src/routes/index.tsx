e07b057f874ea60ab4cd554570c505b409299d91
lubnniamorais <lubnnia@gmail.com>
2025-07-02 10:14:45 -0300

    chore: Add method to include notificação in foreground

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { NotificationWillDisplayEvent, OneSignal } from 'react-native-onesignal';
 
 import { AppRoutes } from './app.routes';
 
@@ -8,6 +10,22 @@ export function Routes() {
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
+
+  useEffect(() => {
+    const handleNotification = (event: NotificationWillDisplayEvent): void => {
+      // Previne do comportamento padrão
+      event.preventDefault();
+
+      // Pegando os detalhes da notificação
+      const response = event.getNotification()
+    }
+
+    // Adicionando a função de notificação
+    OneSignal.Notifications.addEventListener('foregroundWillDisplay', handleNotification);
+
+    // Removendo a função quando o componente é desmontado
+    return () => OneSignal.Notifications.removeEventListener('foregroundWillDisplay', handleNotification)
+  }, []);
 
   return (
     <NavigationContainer theme={theme}>
