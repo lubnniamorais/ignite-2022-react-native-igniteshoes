7645df6ac16462b4a6a5ed69cffcdc35d14b2f66
lubnniamorais <lubnnia@gmail.com>
2025-07-08 10:06:12 -0300

    chore: Implemented function handleNotificationClick

--- App.tsx
+++ App.tsx
@@ -1,54 +1,66 @@
 import {
-	Roboto_400Regular,
-	Roboto_700Bold,
-	useFonts,
-} from "@expo-google-fonts/roboto";
-import { NativeBaseProvider } from "native-base";
-import { useEffect } from "react";
-import { StatusBar } from "react-native";
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+import { NativeBaseProvider } from 'native-base';
+import { useEffect } from 'react';
+import { StatusBar } from 'react-native';
 
-import { type NotificationClickEvent, OneSignal } from "react-native-onesignal";
-import { Loading } from "./src/components/Loading";
-import { CartContextProvider } from "./src/contexts/CartContext";
-import { tagUserInfoCreate } from "./src/notifications/notificationsTags";
-import { Routes } from "./src/routes";
-import { THEME } from "./src/theme";
+import { type NotificationClickEvent, OneSignal } from 'react-native-onesignal';
+import { Loading } from './src/components/Loading';
+import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { Routes } from './src/routes';
+import { THEME } from './src/theme';
 
 // Antes de exportarmos a função devemos inicializar o OneSignal passando o app id
-OneSignal.initialize("5ca1c259-ed23-418e-a1fe-464cce22ea58");
+OneSignal.initialize('5ca1c259-ed23-418e-a1fe-464cce22ea58');
 
 // Antes de exportarmos a função devemos solicitar a permissao para receber notificacoes
 OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
-	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-	tagUserInfoCreate();
+  tagUserInfoCreate();
 
-	useEffect(() => {
-		const handleNotificationClick = (event: NotificationClickEvent): void => {
-			console.log(event);
-		};
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent): void => {
+      const { actionId } = event.result;
 
-		OneSignal.Notifications.addEventListener("click", handleNotificationClick);
+      switch (actionId) {
+        case '1':
+          console.log('Ver todos');
+          break;
+        case '2':
+          console.log('Ver pedido');
+          break;
+        default:
+          console.log('Nenhum botão de ação selecionado.');
+          break;
+      }
+    };
 
-		return () =>
-			OneSignal.Notifications.removeEventListener(
-				"click",
-				handleNotificationClick,
-			);
-	}, []);
+    OneSignal.Notifications.addEventListener('click', handleNotificationClick);
 
-	return (
-		<NativeBaseProvider theme={THEME}>
-			<StatusBar
-				barStyle="light-content"
-				backgroundColor="transparent"
-				translucent
-			/>
-			<CartContextProvider>
-				{fontsLoaded ? <Routes /> : <Loading />}
-			</CartContextProvider>
-		</NativeBaseProvider>
-	);
+    return () =>
+      OneSignal.Notifications.removeEventListener(
+        'click',
+        handleNotificationClick
+      );
+  }, []);
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <CartContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </CartContextProvider>
+    </NativeBaseProvider>
+  );
 }
