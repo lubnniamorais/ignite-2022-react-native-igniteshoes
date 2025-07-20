9ae4dc9554241f22de8a56aa34d657600f68b26a
lubnniamorais <lubnnia@gmail.com>
2025-07-20 16:00:17 -0300

    chore: Finish application

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,69 +1,59 @@
-import { Ionicons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from '@expo/vector-icons';
+import * as Linking from 'expo-linking';
 import {
-	CloseIcon,
-	HStack,
-	Icon,
-	IconButton,
-	Pressable,
-	Text,
-} from "native-base";
-import type { OSNotification } from "react-native-onesignal";
+  CloseIcon,
+  HStack,
+  Icon,
+  IconButton,
+  Pressable,
+  Text,
+} from 'native-base';
+import type { OSNotification } from 'react-native-onesignal';
 
 type Props = {
-	data: OSNotification;
-	onClose: () => void;
-};
-
-type AdditionalDataProps = {
-	route?: string;
-	product_id?: string;
+  data: OSNotification;
+  onClose: () => void;
 };
 
 export function Notification({ data, onClose }: Props) {
-	const { navigate } = useNavigation();
+  function handleOnPress() {
+    if (data.launchURL) {
+      Linking.openURL(data.launchURL);
+    }
+  }
 
-	function handleOnPress() {
-		const { route, product_id } = data.additionalData as AdditionalDataProps;
+  return (
+    <Pressable
+      w='full'
+      p={4}
+      pt={12}
+      bgColor='gray.200'
+      position='absolute'
+      top={0}
+      onPress={handleOnPress}
+    >
+      <HStack justifyContent='space-between' alignItems='center'>
+        <Icon
+          as={Ionicons}
+          name='notifications-outline'
+          size={5}
+          color='black'
+          mr={2}
+        />
 
-		if (route === "details" && product_id) {
-			navigate("details", { productId: product_id });
-			onClose();
-		}
-	}
+        <Text fontSize='md' color='black' flex={1}>
+          {data.title}
+        </Text>
 
-	return (
-		<Pressable
-			w="full"
-			p={4}
-			pt={12}
-			bgColor="gray.200"
-			position="absolute"
-			top={0}
-			onPress={handleOnPress}
-		>
-			<HStack justifyContent="space-between" alignItems="center">
-				<Icon
-					as={Ionicons}
-					name="notifications-outline"
-					size={5}
-					color="black"
-					mr={2}
-				/>
-
-				<Text fontSize="md" color="black" flex={1}>
-					{data.title}
-				</Text>
-
-				<IconButton
-					variant="unstyled"
-					_focus={{ borderWidth: 0 }}
-					icon={<CloseIcon size="3" />}
-					_icon={{ color: "coolGray.600" }}
-					color="black"
-					onPress={onClose}
-				/>
-			</HStack>
-		</Pressable>
-	);
+        <IconButton
+          variant='unstyled'
+          _focus={{ borderWidth: 0 }}
+          icon={<CloseIcon size='3' />}
+          _icon={{ color: 'coolGray.600' }}
+          color='black'
+          onPress={onClose}
+        />
+      </HStack>
+    </Pressable>
+  );
 }
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,55 +1,57 @@
-import { Feather, Ionicons } from "@expo/vector-icons";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather, Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { useTheme } from "native-base";
-
-import { Cart } from "../screens/Cart";
-import { Details } from "../screens/Details";
-import { Home } from "../screens/Home";
+import { useTheme } from 'native-base';
+import { useCart } from '../hooks/useCart';
+import { Cart } from '../screens/Cart';
+import { Details } from '../screens/Details';
+import { Home } from '../screens/Home';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
-	const { colors, sizes } = useTheme();
+  const { colors, sizes } = useTheme();
+  const { cart } = useCart();
 
-	return (
-		<Navigator
-			screenOptions={{
-				headerShown: false,
-				tabBarShowLabel: false,
-				tabBarActiveTintColor: colors.green[500],
-				tabBarInactiveTintColor: colors.gray[300],
-				tabBarStyle: {
-					borderTopWidth: 0,
-					backgroundColor: colors.gray[800],
-				},
-			}}
-		>
-			<Screen
-				name="products"
-				component={Home}
-				options={{
-					tabBarIcon: ({ color }) => (
-						<Ionicons name="home-outline" color={color} size={sizes[6]} />
-					),
-				}}
-			/>
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.green[500],
+        tabBarInactiveTintColor: colors.gray[300],
+        tabBarStyle: {
+          borderTopWidth: 0,
+          backgroundColor: colors.gray[800],
+        },
+      }}
+    >
+      <Screen
+        name='products'
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name='home-outline' color={color} size={sizes[6]} />
+          ),
+        }}
+      />
 
-			<Screen
-				name="cart"
-				component={Cart}
-				options={{
-					tabBarIcon: ({ color }) => (
-						<Feather name="shopping-bag" color={color} size={sizes[6]} />
-					),
-				}}
-			/>
+      <Screen
+        name='cart'
+        component={Cart}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name='shopping-bag' color={color} size={sizes[6]} />
+          ),
+          tabBarBadge: cart.length > 0 ? cart.length : undefined,
+        }}
+      />
 
-			<Screen
-				name="details"
-				component={Details}
-				options={{ tabBarItemStyle: { display: "none" } }}
-			/>
-		</Navigator>
-	);
+      <Screen
+        name='details'
+        component={Details}
+        options={{ tabBarItemStyle: { display: 'none' } }}
+      />
+    </Navigator>
+  );
 }
